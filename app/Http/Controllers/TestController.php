61d36251dd95cc4933b206195493ab1a749924a5
Abdel Kader <maguiskader0@gmail.com>
2021-06-08 17:02:42 +0000

    firstcommit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Types_caissons;
+use App\Models\Types_vehicules;
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index(){
+        $types_vehicules = Types_vehicules::all();
+        $types_caissons = Types_caissons::all();
+
+    }
+
+
+
+        function csvToArray($filename = '', $delimiter = ','){
+
+            if (!file_exists($filename) || !is_readable($filename))
+                return false;
+
+            $header = null;
+            $data = array();
+            if (($handle = fopen($filename, 'r')) !== false)
+            {
+                while (($row = fgetcsv($handle, 1000, $delimiter)) !== false)
+                {
+                    if (!$header)
+                        $header = $row;
+                    else
+                        $data[] = array_combine($header, $row);
+                }
+                fclose($handle);
+            }
+
+            return $data;
+        }
+
+        public function importCsv(){
+            $file = public_path('file/test.csv');
+
+            $customerArr = $this->csvToArray($file);
+
+            for ($i = 0; $i < count($customerArr); $i ++)
+            {
+                User::firstOrCreate($customerArr[$i]);
+            }
+
+            return 'Jobi done or what ever';
+        }
+
+}
